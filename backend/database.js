68b1b377632096cafbf51b2f79ec4fc35267f1e8
emanuelvidall <emanuelvidalrm@gmail.com>
2023-04-17 15:55:21 -0300

    fixing/testing backend route

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,8 @@
-//conectando com mongodb local da maquina
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const url = `mongodb+srv://emanuelvidalrm:${process.env.DATABASE_PASSWORD}@appzeusdb.lq9izu5.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.connect('mongodb://localhost:27017/nodeapp', {useNewUrlParser: true});
+mongoose.connect(url, {useNewUrlParser: true});
 
 const connectionParams={
     useNewUrlParser: true,
@@ -17,5 +16,3 @@ mongoose.connect(url,connectionParams)
     .catch( (err) => {
         console.error(`Error connecting to the database. \n${err}`);
     })
-
-module.exports = conn;
