6f96579e7c475659b5e76164d8b59497d49ae243
emanuelvidall <emanuelvidalrm@gmail.com>
2023-04-17 15:41:17 -0300

    connection to database atlas

--- backend/database.js
+++ backend/database.js
@@ -1,16 +1,21 @@
 //conectando com mongodb local da maquina
 var mongoose = require('mongoose');
+
+const url = `mongodb+srv://emanuelvidalrm:${process.env.DATABASE_PASSWORD}@appzeusdb.lq9izu5.mongodb.net/?retryWrites=true&w=majority`
+
 mongoose.connect('mongodb://localhost:27017/nodeapp', {useNewUrlParser: true});
 
-var conn = mongoose.connection;
-
-conn.on('connected', function() {
-    console.log('database is connected successfully');
-});
-conn.on('disconnected',function(){
-    console.log('database is disconnected successfully');
-})
-
-conn.on('error', console.error.bind(console, 'connection error:'));
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
 
 module.exports = conn;
